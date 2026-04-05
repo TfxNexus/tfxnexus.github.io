f231d0afcbb9baf9cf85fa5166def2ca4c39b8e5
TfxNexus <67941064+TfxNexus@users.noreply.github.com>
2026-04-05 22:01:50 +0700

    Auto-save: update source files

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,25 +1,33 @@
 import { Routes, Route } from 'react-router-dom'
 import { NavBar } from './components/NavBar'
+import { LeftSidebar, RightSidebar } from './components/PageSidebar'
 import { HomePage } from './pages/HomePage'
 import { AboutPage } from './pages/AboutPage'
 import { ProjectsPage } from './pages/ProjectsPage'
 import { ResumePage } from './pages/ResumePage'
+import { HistoryPage } from './pages/HistoryPage'
 import { NotFoundPage } from './pages/NotFoundPage'
 
 export default function App() {
   return (
-    <div className="min-h-screen bg-gray-900 dark:bg-gray-900 text-gray-100 dark:text-gray-100
-                    [html:not(.dark)_&]:bg-gray-50 [html:not(.dark)_&]:text-gray-900">
+    <div className="min-h-screen bg-gray-900 text-gray-100">
       <NavBar />
-      <main>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/about" element={<AboutPage />} />
-          <Route path="/projects" element={<ProjectsPage />} />
-          <Route path="/resume" element={<ResumePage />} />
-          <Route path="*" element={<NotFoundPage />} />
-        </Routes>
-      </main>
+      <div className="max-w-screen-2xl mx-auto px-4 sm:px-6 xl:px-8">
+        <div className="flex gap-6 xl:gap-8 py-0">
+          <LeftSidebar />
+          <main className="flex-1 min-w-0">
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/about" element={<AboutPage />} />
+              <Route path="/projects" element={<ProjectsPage />} />
+              <Route path="/resume" element={<ResumePage />} />
+              <Route path="/history" element={<HistoryPage />} />
+              <Route path="*" element={<NotFoundPage />} />
+            </Routes>
+          </main>
+          <RightSidebar />
+        </div>
+      </div>
     </div>
   )
 }
